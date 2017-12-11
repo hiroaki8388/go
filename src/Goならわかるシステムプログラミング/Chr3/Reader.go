d9e7a3b853bffebc08fd66b9683ab7bd2ebedc30
hasegawa8388 <hasegawahiroaki@hasegawanomacbookAir-2.local>
2017-12-11 23:49:13 +0900

    Readerファイルの追加

---
+++ src/Goならわかるシステムプログラミング/Chr3/Reader.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func FileReader(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	io.Copy(os.Stdout, file)
+}
+func main() {
+	// ファイルの中身を標準出力する
+	FileReader("src/resouce/output.txt")
+}
